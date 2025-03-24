f6db9be944ccfa9768adf2244bd140def575026d
faibles <maria0987051500@gmail.com>
2025-03-25 00:18:19 +0200

    Initial commit

---
+++ patientsRegistrationSystem/src/main/java/com/example/patientsRegistrationSystem/PatientsRegistrationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.patientsRegistrationSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientsRegistrationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatientsRegistrationSystemApplication.class, args);
+	}
+
+}
---
+++ patientsRegistrationSystem/src/test/java/com/example/patientsRegistrationSystem/PatientsRegistrationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.patientsRegistrationSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientsRegistrationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
